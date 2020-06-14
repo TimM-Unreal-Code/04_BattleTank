c4b6fc80d1fcc18ae8b390651f8aa018026a4478
schmildo <schmildo@gmail.com>
2020-06-14 14:08:02 +1000

    186 - Creating & Deleteing Landscapes

---
+++ UnrealBattleTank/Source/UnrealBattleTank/UnrealBattleTankGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBATTLETANK_API AUnrealBattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
